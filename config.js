333b2cf29d6d49784faea32d16cb968db52771c3
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-09-20 19:06:25 -0400

    change site metadata

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://iuliupop.com',
   pathPrefix: '/',
   title: 'Blog by Iuliu Pop',
   subtitle: 'Learning software engineering in the open.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: '',
   useKatex: false,
   menu: [
     {
